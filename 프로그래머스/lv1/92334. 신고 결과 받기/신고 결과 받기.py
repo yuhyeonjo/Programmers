79f5db6a1f444f8bf44c3dfe1e07d3909a1b5bb5
yuhyeonjo <83911109+yuhyeonjo@users.noreply.github.com>
2023-08-01 17:40:44 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -2,21 +2,33 @@ import collections
 
 def solution(id_list, report, k):
     answer = []
-    report = list(set(report))
+    report_list = []
+#     id_list = dict.fromkeys(id_list, 0)
+
+    # 신고한 이용자 ID 정보 문자열 배열 공백 나누기
+    for i in list(set(report)) :
+        temp = i.split(" ")
+        report_list.append(temp)
+    
+#     # 유저별 신고당한 횟수 구하기
+#     for report_name in report_list :
+#         for key, value in id_list.items() :
+#             if(key == report_name[1]) :
+#                 id_list[key] += 1
+    
+
     reportHash = collections.defaultdict(set)
     stoped = collections.defaultdict(int)
     
-    # 유저별 신고한 ID와 신고 횟수 구하기 
-    for x in report :
-        a, b = x.split(" ")
-        reportHash[a].add(b)
-        stoped[b]+=1
-        
-    # 신고 횟수 구하기
-    for name in id_list :
-        count = 0
-        for user in reportHash[name] :        
-            if(stoped[user] >= k) :
-                count += 1
-        answer.append(count)
+    for a in report_list :
+        reportHash[a[0]].add(a[1])
+        # stoped[b]+=1
+    
+    for b in reportHash :
+        print(b)
+    
+    # print(reportHash)
+    # print(stoped)
+                
+    
     return answer
